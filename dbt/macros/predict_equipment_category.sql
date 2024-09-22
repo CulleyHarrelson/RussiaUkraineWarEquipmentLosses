1f4652d3d0ce061d712dce9b26e3db1f22776d3a
culley <harrelson@gmail.com>
2024-09-22 15:40:17 -0700

    updating categories

--- dbt/macros/predict_equipment_category.sql
+++ dbt/macros/predict_equipment_category.sql
@@ -6,14 +6,14 @@
         WHEN {{ column_name }} ~* 'C-\d+|Il-\d+|An-\d+|KC-\d+|A\d+\d+M|Hercules' THEN 'Transport Plane'
         WHEN {{ column_name }} ~* 'E-\d+|RC-\d+|U-2|SR-71|AWACS|Hawkeye' THEN 'Reconnaissance Aircraft'
         WHEN {{ column_name }} ~* 'AH-\d+|Ka-\d+|Mi-\d+|Apache|Cobra|Hind' THEN 'Attack Helicopter'
-        WHEN {{ column_name }} ~* 'UH-\d+|CH-\d+|Mi-8|Chinook|Blackhawk' THEN 'Transport Helicopter'
+        WHEN {{ column_name }} ~* 'UH-\d+|CH-\d+|Mi-8|Chinook|Blackhawk|Helecopter' THEN 'Transport Helicopter'
         WHEN {{ column_name }} ~* 'UAV|RQ-\d+|MQ-\d+|Predator|Reaper|Global Hawk|Bayraktar|Orlan' THEN 'UAV/Drone'
         WHEN {{ column_name }} ~* 'A-\d+|Su-25|Warthog|Frogfoot' THEN 'Ground Attack Aircraft'
         WHEN {{ column_name }} ~* 'P-\d+|P-8|Tu-142|Orion|Poseidon' THEN 'Maritime Patrol Aircraft'
         WHEN {{ column_name }} ~* 'EA-\d+|EF-\d+|Growler|Prowler' THEN 'Electronic Warfare Aircraft'
 
         /* Ground Vehicles */
-        WHEN {{ column_name }} ~* 'T-\d+|Abrams|Leopard|Challenger|Merkava' THEN 'Main Battle Tank'
+        WHEN {{ column_name }} ~* 'T-\d+|Abrams|Leopard|Challenger|Merkava' THEN 'Tank'
         WHEN {{ column_name }} ~* 'BMP|Bradley|Warrior|Puma' THEN 'Infantry Fighting Vehicle'
         WHEN {{ column_name }} ~* 'M113|BTR|Stryker|VAB' THEN 'Armored Personnel Carrier'
         WHEN {{ column_name }} ~* 'M109|PzH|CAESAR|Archer' THEN 'Self-Propelled Artillery'
@@ -39,7 +39,7 @@
 
         /* Electronic Warfare Equipment */
         WHEN {{ column_name }} ~* 'Krasukha|Murmansk-BN|EA-18G' THEN 'Electronic Warfare System'
-        WHEN {{ column_name }} ~* 'AN/SPY-1|AESA|Nebo' THEN 'Radar System'
+        WHEN {{ column_name }} ~* 'AN/SPY-1|AESA|Nebo|radar' THEN 'Radar System'
 
         /* Command and Control Systems */
         WHEN {{ column_name }} ~* 'AWACS|E-3|A-50' THEN 'Airborne Early Warning and Control'
@@ -55,6 +55,32 @@
         WHEN {{ column_name }} ~* 'AAV-7|BMP-3F|Type 05' THEN 'Amphibious Assault Vehicle'
         WHEN {{ column_name }} ~* 'LCAC|LCU|Type 726' THEN 'Landing Craft'
 
-        ELSE 'Other Military Equipment'
+
+        WHEN {{ column_name }} ~* 'T-\d+|Unknown tank' THEN 'Tank'
+        WHEN {{ column_name }} ~* 'BMP-\d+|BMD-\d+|BRM-\d+' THEN 'Infantry Fighting Vehicle'
+        WHEN {{ column_name }} ~* 'MT-LB|BTR-\d+' THEN 'Armored Personnel Carrier'
+        WHEN {{ column_name }} ~* '2S1|2S3|2S5|2S7|2S9|2S19|2S22|2S23|2S33|2S34' THEN 'Howitzer'
+        WHEN {{ column_name }} ~* 'D-20|D-30|D-44|2A36|2A65|M777|FH70|TRF1' THEN 'Howitzer'
+        WHEN {{ column_name }} ~* '2B9|2B11|2B16|2S12|M120K' THEN 'Mortar'
+        WHEN {{ column_name }} ~* 'BM-21|BM-27|BM-30|TOS-1A|2B17|RM-70' THEN 'Multiple Rocket Launcher'
+        WHEN {{ column_name }} ~* 'Pantsir|Tor|Buk|S-300|S-400' THEN 'Air Defense System'
+        WHEN {{ column_name }} ~* 'GAZ Tigr|International MaxxPro|Mastiff|Wolfhound|Husky|Kozak|Roshel Senator|MaxxPro' THEN 'MRAP'
+        WHEN {{ column_name }} ~* 'UAZ-469|HMMWV' THEN 'Light Utility Vehicle'
+        WHEN {{ column_name }} ~* 'Ural-\d+|KamAZ|GAZ-\d+|KrAZ|truck|ural' THEN 'Truck'
+        WHEN {{ column_name }} ~* 'BREM-\d+|IMR-\d+|MTP-\d+' THEN 'Engineering Vehicle'
+        WHEN {{ column_name }} ~* 'armoured\ recovery\ vehicle' THEN 'Armoured Recovery Vehicle'
+        WHEN {{ column_name }} ~* 'R-\d+|1L\d+|9S\d+|forward\ observer\ vehicle|battery\ fire\ control\ center' THEN 'Command and Control Vehicle'
+        WHEN {{ column_name }} ~* 'Mi-\d+|Ka-\d+|AH-\d+|UH-\d+' THEN 'Helicopter'
+        WHEN {{ column_name }} ~* 'L-39|Su-\d+|MiG-\d+' THEN 'Combat Aircraft'
+        WHEN {{ column_name }} ~* 'TB2|Orlan-\d+|Mohajer-\d+|Forpost|ScanEagle' THEN 'UAV/Drone'
+        WHEN {{ column_name }} ~* '9K\d+|9M\d+|Metis|Kornet|Konkurs' THEN 'Anti-Tank Guided Missile'
+        WHEN {{ column_name }} ~* 'PMP|TMM-\d+' THEN 'Bridge-Laying Equipment'
+        WHEN {{ column_name }} ~* 'Project \d+|Class' THEN 'Naval Vessel'
+        WHEN {{ column_name }} ~* '155mm|howitzer|2A65 Msta-B|2S1 Gvozdika|2S3 Akatsiya|2S19 Msta-S|M109|M777|FH70|D-20|D-30|2S22 Bohdana|ShKH vz\. 77 DANA|AHS Krab|PzH 2000|G6|CAESAR|Archer' THEN 'Howitzer'
+        WHEN {{ column_name }} ~* 'van|vehicle' THEN 'Unknown Vehicle'
+
+
+
+        ELSE 'Unknown'
     END
 {% endmacro %}
